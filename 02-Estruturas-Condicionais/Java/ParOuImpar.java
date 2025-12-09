a26c6c0aa16d34d23056d9abd8c29a31fcf195a5
Maria-ECAA <mcaa7476@gmail.com>
2025-12-09 20:28:12 -0300

    Create ParOuImpar.java
    Adicionado Ex 2.1: Par ou Ímpar em Java

---
+++ 02-Estruturas-Condicionais/Java/ParOuImpar.java
@@ -0,0 +1,16 @@
+// Objetivo: Verificar se um número é par ou ímpar usando o operador Módulo (%)
+
+class ParOuImpar {
+    public static void main(String[] args) {
+        int numero = 7; // Variável que será testada
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é Par.");
+        } else {
+            System.out.println("O número é Ímpar.");
+        }
+    }
+}
+
+// Conceito: Diferente do JavaScript, o Java exige a declaração do tipo (int).
+// Dica de Sintaxe: O código deve estar dentro de uma classe e do método 'main'.
