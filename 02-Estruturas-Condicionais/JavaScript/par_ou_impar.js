0d3bc5682272b96ff59fc38d9f6da121cb374053
Maria-ECAA <mcaa7476@gmail.com>
2025-12-09 20:17:05 -0300

    Create par_ou_impar.js
    adicionado Ex 2.1: Par ou Ímpar em JavaScript

---
+++ 02-Estruturas-Condicionais/JavaScript/par_ou_impar.js
@@ -0,0 +1,12 @@
+// Objetivo: Verificar se um número é par ou ímpar usando o operador Módulo (%)
+
+let numero = 20; // Variável que será testada
+
+if (numero % 2 == 0) {
+    console.log("O número é Par.");
+} else {
+    console.log("O número é Ímpar.");
+}
+
+// Conceito: A condição 'numero % 2 == 0' verifica se o resto da divisão por 2 é zero.
+// Dica de Sintaxe: O uso de '==' é crucial para comparação (diferente do '=' para atribuição).
